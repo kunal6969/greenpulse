bf316c0761e5f5ec05d1546953054dfa748e31a7
kunal6969 <kunalthapliyal07@gmail.com>
2025-11-09 16:11:56 +0530

    Remove team section from landing page

---
+++ greenpulse-main/components/landing/LandingPage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import LandingHeader from './LandingHeader';
+import HeroSection from './HeroSection';
+import FeaturesSection from './FeaturesSection';
+import HowItWorksSection from './HowItWorksSection';
+import DifferentiatorsSection from './DifferentiatorsSection';
+import LandingFooter from './LandingFooter';
+import FloatingLeaves from '../ui/FloatingLeaves';
+import FlowingGlints from '../ui/FlowingGlints';
+import AmbientSoundPlayer from '../ui/AmbientSoundPlayer';
+import AuroraBackground from '../ui/AuroraBackground';
+import GlowingOrbs from '../ui/GlowingOrbs';
+import DriftingParticles from '../ui/DriftingParticles';
+
+interface LandingPageProps {
+  onLogin: () => void;
+}
+
+const LandingPage: React.FC<LandingPageProps> = ({ onLogin }) => {
+  return (
+    <div className="bg-bg-primary/80 text-text-primary font-outfit relative">
+      <FloatingLeaves />
+      <FlowingGlints />
+      <AuroraBackground />
+      <GlowingOrbs />
+      <DriftingParticles />
+      <AmbientSoundPlayer />
+      <LandingHeader onLogin={onLogin} />
+      <main>
+        <HeroSection onLogin={onLogin} />
+        <FeaturesSection />
+        <HowItWorksSection />
+        <DifferentiatorsSection />
+      </main>
+      <LandingFooter />
+    </div>
+  );
+};
+
+export default LandingPage;
